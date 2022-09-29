0ce295b120588689dc8e2f1c1e9f10eef9953806
Evan Greenberg <greenberg@Evans-MacBook-Pro.local>
2022-09-28 19:02:18 -0700

    new requirements file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-poly="/Users/greenberg/Documents/PHD/Projects/Mobility/TaiwanMasks/Shapes/Taiwan1_long.gpkg"
+river='Amazonas_Jatuarana'
+poly="/Users/greenberg/Documents/PHD/Projects/Mobility/MethodsPaper/AustinMasks/$river/$river.gpkg"
 mask_method="Jones"     # Jones, Zou, esa
 dataset='landsat'      # landsat, sentinel
-network_method="merit"   # grwl, merit, largest, all
+network_method="grwl"   # grwl, merit, largest, all
 network_path="/Users/greenberg/Documents/PHD/Projects/Mobility/river_networks/channel_networks_full.shp"    # Needs to be on computer
 images="false"   # true, false
 masks="true"    # true, false
@@ -11,7 +12,6 @@ start="01-01"   # Month-Day format with leading 0s
 end="12-31"     # Month-Day format with leading 0s
 start_year="1985"   
 end_year="2021"     
-out="/Users/greenberg/Documents/PHD/Projects/Mobility/TaiwanMasks/Files/"
-river="Taiwan1"
+out="/Users/greenberg/Documents/PHD/Projects/Mobility/MethodsPaper/AustinMasks/"
 
 python ../GEE_watermasks/main.py --poly $poly --mask_method $mask_method --network_method $network_method --network_path $network_path --masks $masks --images $images --dataset $dataset --water_level $water_level --start $start --end $end --start_year $start_year --end_year $end_year --out $out --river $river
